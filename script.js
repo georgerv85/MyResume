11ebf5f3ca017556644a0f7e197118d718c756d2
George Arvanitis <137385184+georgerv85@users.noreply.github.com>
2024-10-16 10:11:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    function linkedin() {
+        window.open('https://www.linkedin.com/in/george-arvanitis-gr/', "_blank");
+    }
+
+    function gitHub() {
+        window.open('https://github.com/georgerv85', "_blank");
+    }
+
